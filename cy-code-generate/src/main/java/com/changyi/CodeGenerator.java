c518ab7462e1df4839531c77103fa1744a9c5756
ljr <15035456537@163.com>
2021-03-16 15:17:17 +0800

    1. 引入mybatis plus项目

---
+++ cy-code-generate/src/main/java/com/changyi/CodeGenerator.java
@@ -0,0 +1,182 @@
+package com.changyi;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CodeGenerator {
+
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+    //避免手动收入因为都是一个模块里面，不包含多个模块
+    //需要生产的表名
+    public static final String[] TABLE_LIST = {"tb_item"};
+    //模块名
+    public static final String MODULE_NAME = "test";
+    public static void main(String[] args) {
+        // cy-gateway器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        final String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/cy-gateway/src/main/java");
+        gc.setAuthor("Robot.Chy");
+        gc.setXmlName("%sMapper");
+        gc.setOpen(false);
+        //实体属性 Swagger2 注解
+        gc.setSwagger2(true);
+
+        //开启 baseColumnList
+        gc.setBaseColumnList(true);
+
+        //开启 BaseResultMap
+        gc.setBaseResultMap(true);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/clouddemo?useUnicode=true&characterEncoding=utf8&useOldAliasMetadataBehavior=true&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true&serverTimezone=UTC");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("123456");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        final PackageConfig pc = new PackageConfig();
+//        pc.setModuleName(scanner("模块名"));
+        //取消手动收入，直接写死
+        pc.setModuleName(MODULE_NAME);
+        pc.setParent("com.changyi.chy");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        //String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+        String templatePath = "templates/mapperTemplate.xml.vm";
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/cy-gateway/src/main/resources/mapper/"
+                        + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+
+        // 自定义请求添加实体类
+        String reqAddtemplatePath = "templates/reqAddEntityTemplate.java.vm";
+        focList.add(new FileOutConfig(reqAddtemplatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                return projectPath + "/cy-gateway/src/main/java/"+ pc.getParent().replace(".","/") +"/"+ "request/"
+                        + "Req" + tableInfo.getEntityName()+"Add" + StringPool.DOT_JAVA;
+            }
+        });
+
+        // 自定义请求更新实体类
+        String reqUpdatetemplatePath = "templates/reqUpdateEntityTemplate.java.vm";
+        focList.add(new FileOutConfig(reqUpdatetemplatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                return projectPath + "/cy-gateway/src/main/java/"+ pc.getParent().replace(".","/") +"/"+ "request/"
+                        + "Req" + tableInfo.getEntityName()+"Update" + StringPool.DOT_JAVA;
+            }
+        });
+        // 自定义请求查询实体类
+        String reqQuerytemplatePath = "templates/reqQueryEntityTemplate.java.vm";
+        focList.add(new FileOutConfig(reqQuerytemplatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                return projectPath + "/cy-gateway/src/main/java/"+ pc.getParent().replace(".","/") +"/"+ "request/"
+                        + "Req" + tableInfo.getEntityName()+"Query" + StringPool.DOT_JAVA;
+            }
+        });
+
+        // 自定义请求返回实体类
+        String ResponseEntitytemplatePath = "templates/responseEntityTemplate.java.vm";
+        focList.add(new FileOutConfig(ResponseEntitytemplatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/cy-gateway/src/main/java/"+pc.getParent().replace(".","/") +"/"+"/"+"response/"
+                        + "Response" + tableInfo.getEntityName() + StringPool.DOT_JAVA;
+            }
+        });
+
+
+
+
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        templateConfig.setController("templates/controllerTemplate.java");
+        templateConfig.setEntity("templates/entityTemplate.java");
+        templateConfig.setEntityKt("templates/entityTemplate.kt");
+        templateConfig.setMapper("templates/mapperTemplate.java");
+        templateConfig.setService("templates/serviceTemplate.java");
+        templateConfig.setServiceImpl("templates/serviceImplTemplate.java");
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+//        strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        // 公共父类
+//        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // 写于父类中的公共字段
+        //strategy.setSuperEntityColumns("id");
+//        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setInclude(TABLE_LIST);
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        //mpg.setTemplateEngine(new VelocityTemplateEngine());
+        mpg.execute();
+    }
+}
