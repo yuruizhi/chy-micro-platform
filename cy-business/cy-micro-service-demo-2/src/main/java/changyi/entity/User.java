aa1e6747b73e071201fa9a5cccae451c3076b18e
ZhangHao <376981735@qq.com>
2021-03-17 14:38:28 +0800

    :white_check_mark: 测试统一异常和结果返回

---
+++ cy-business/cy-micro-service-demo-2/src/main/java/changyi/entity/User.java
@@ -0,0 +1,16 @@
+package demo;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+
+@Data
+public class User {
+    @NotNull(message = "用户名不能为空")
+    private String name;
+
+    @NotBlank(message = "密码不能为空")
+    private String password;
+}
