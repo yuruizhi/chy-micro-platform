445a3b9b5cde82f0ee8ff2b7642fa67def942e65
Henry.Yu <282373647@qq.com>
2021-03-22 16:53:10 +0800

    解决redis连接异常

--- cy-business/cy-micro-service-demo-1/src/main/java/com/changyi/demo1/ServiceDemo1.java
+++ cy-business/cy-micro-service-demo-1/src/main/java/com/changyi/demo1/ServiceDemo1.java
@@ -1,14 +1,13 @@
 package com.changyi.demo1;
 
+import com.changyi.cloud.dispose.starter.annotation.EnableGlobalDispose;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RestController
+@EnableGlobalDispose
 public class ServiceDemo1 {
 
     public static void main(String[] args) {
--- cy-business/cy-micro-service-demo-1/src/main/java/com/changyi/demo1/controller/DemoController.java
+++ cy-business/cy-micro-service-demo-1/src/main/java/com/changyi/demo1/controller/DemoController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +22,13 @@ public class DemoController {
 
 
     @ApiOperation(value = "Demo1 api")
-    @RequestMapping("/t1")
+    @GetMapping("/t1")
     public String home() {
         return "demo1";
     }
 
 
-    @RequestMapping("/t2")
+    @GetMapping("/t2")
     public String t2() {
 
         log.debug("demo11111111111111");
